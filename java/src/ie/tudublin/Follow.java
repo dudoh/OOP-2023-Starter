8b70fd3b05f19d3ca16730a9fae8372d78a8454f
Dudoh <c21455002@mytudublin.ie>
2023-04-26 18:32:50 +0100

    commit 1

---
+++ java/src/ie/tudublin/Follow.java
@@ -0,0 +1,37 @@
+//C21455002 - DAVID UDOH
+
+package ie.tudublin;
+
+public class Follow {
+    private String word;
+    private int count;
+
+    public Follow(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void Word(String word) {
+        this.word = word;
+    }
+
+    public void CountWord(int count) {
+        this.count = count;
+    }
+
+    public void incrementCount() {
+        this.count++;
+    }
+
+
+}
+
+
